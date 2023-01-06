b3074f71202b721d7225cc484a35639ad39342f9
Mila <blackcatstone@gmail.com>
2023-01-06 22:56:51 +0000

    Add exclusions and settings tab to configure them

--- main.ts
+++ main.ts
@@ -1,52 +1,122 @@
-import { CachedMetadata, Plugin, TAbstractFile, TFile } from 'obsidian';
+import { App, Plugin, PluginSettingTab, Setting, TAbstractFile } from 'obsidian';
 import { kebabCase } from 'lodash';
 import * as path from 'path';
 
+interface FilenameKebabSettings {
+	onlyNotes: boolean,
+	excludeByPrefix: boolean,
+	exclusionPrefix: string,
+}
+
+const DEFAULT_SETTINGS: FilenameKebabSettings = {
+	onlyNotes: false,
+	excludeByPrefix: true,
+	exclusionPrefix: '_',
+}
+
 export default class FilenameKebab extends Plugin {
+	settings: FilenameKebabSettings;
+	
 	async onload() {
-		this.registerEvent(
-			this.app.metadataCache.on('changed', async (file, data, cache) => {
-				await updateFileName({ plugin: this, file, cache });
-			})
-		);
+		await this.loadSettings();
+		this.addSettingTab(new FilenameKebabSettingsTab(this.app, this));
 
 		this.registerEvent(
 			this.app.vault.on('rename', async (file) => {
-				await updateNewFileName({ plugin: this, file});
+				await this.updateFileName(file);
+			})
+		);
+
+		this.registerEvent(
+			this.app.vault.on('create', async (file) => {
+				await this.updateFileName(file);
 			})
 		);
 	}
+
+	async loadSettings() {
+		this.settings = Object.assign({}, DEFAULT_SETTINGS, await this.loadData());
+	}
+	
+	async saveSettings() {
+		await this.saveData(this.settings);
+	}
+
+	async updateFileName( file: TAbstractFile) {
+		if (this.isExcluded(file)) return;
+
+		const dir = path.parse(file.path).dir
+		const fileName = path.parse(file.path).name;
+		const ext = path.parse(file.path).ext;
+
+		const newFileName = path.join(dir, `${kebabCase(fileName)}${ext}`);
+		if (file.name !== fileName) {
+			await this.app.fileManager.renameFile(file, newFileName);
+		}
+	}
+
+	isExcluded(file: TAbstractFile): boolean {
+		if (file.path.startsWith('.')) {
+			// The plugin will not try to rename UNix-style hidden files.
+			return true;
+		}
+		
+		if (this.settings.excludeByPrefix && file.path.startsWith(this.settings.exclusionPrefix)) {
+			return true;
+		}
+
+		if (this.settings.onlyNotes) {
+			const ext = path.parse(file.name).ext;
+			return ext !== '.md';
+		}
+
+		return false;
+	}
 }
 
-const updateFileName = async ({
-	plugin,
-	file,
-	cache,
-}: {
+class FilenameKebabSettingsTab extends PluginSettingTab {
 	plugin: FilenameKebab;
-	file: TFile;
-	cache: CachedMetadata;
-}) => {
-	const [{ heading: title }] = cache?.headings ?? [{ heading: file.basename }];
 
-	const fileName = `${kebabCase(title)}.md`;
-	if (file.basename !== fileName) {
-		await plugin.app.fileManager.renameFile(file, fileName);
+	constructor(app: App, plugin: FilenameKebab) {
+		super(app, plugin);
+		this.plugin = plugin;
 	}
-};
 
-const updateNewFileName = async ({
-	plugin,
-	file,
-}: {
-	plugin: FilenameKebab;
-	file: TAbstractFile;
-}) => {
-	const fileName = path.parse(file.name).name;
-	const ext = path.parse(file.name).ext;
+	display(): void {
+		const { containerEl } = this;
 
-	const newFileName = `${kebabCase(fileName)}${ext}`;
-	if (file.name !== fileName) {
-		await plugin.app.fileManager.renameFile(file, newFileName);
+		containerEl.empty();
+
+		containerEl.createEl('h2', { text: 'Settings for Filename Kebab plugin.' });
+
+		new Setting(containerEl)
+			.setName('Only change notes')
+			.setDesc('The plugin will only change the filenames of notes.')
+			.addToggle(toggle => toggle
+				.setValue(this.plugin.settings.onlyNotes)
+				.onChange(async (value) => {
+					this.plugin.settings.onlyNotes = value;
+					await this.plugin.saveSettings();
+				}));
+
+		new Setting(containerEl)
+			.setName('Exclude files with prefix')
+			.setDesc("The plugin will not change the filenames of Unix-style hidden files.")
+			.addToggle(toggle => toggle
+				.setValue(this.plugin.settings.excludeByPrefix)
+				.onChange(async (value) => {
+					this.plugin.settings.excludeByPrefix = value;
+					await this.plugin.saveSettings();
+				}));
+
+		new Setting(containerEl)
+			.setName('Prefix:')
+			.setDesc('Filenames beginning with this prefix will not be changed.')
+			.addText(text => text
+				.setValue(this.plugin.settings.exclusionPrefix)
+				.onChange(async (value) => {
+					this.plugin.settings.exclusionPrefix = value;
+					await this.plugin.saveSettings();
+				}));
 	}
-};
+}
